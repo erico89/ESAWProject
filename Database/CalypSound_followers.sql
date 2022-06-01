5ce46860e76cdeb85e45686b7b40e9e27a920ceb
CristinaMendoza01 <cristina.mendoza01@estudiant.upf.edu>
2022-06-01 20:27:22 +0200

    Cambio

--- Database/CalypSound_followers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `followers` (
-  `user_id` INT NOT NULL,
-  `follower_id` INT NOT NULL,
-  PRIMARY KEY (`user_id`, `follower_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `follower_id` FOREIGN KEY (`follower_id`) REFERENCES `users` (`id`)
-)
-ENGINE = InnoDB
