5f03d385f526008cb816eda955015b776bbc3aea
erico89 <72563708+erico89@users.noreply.github.com>
2022-06-18 18:08:35 +0200

    Revert "Merge branch 'main' of https://github.com/erico89/ESAWProject"
    This reverts commit 1b91c679b0d4dcd53665cdea1bd913a2b576b713, reversing
    changes made to ec44ba6e813f149726d7b7cd5d5689c1d21b37a4.

--- Database/CalypSounnd_db_likes.sql
+++ Database/CalypSounnd_db_likes.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `likes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 
-CREATE TABLE `Likes` (
+CREATE TABLE `likes` (
   `tweet_id` INT NOT NULL,
   `user_id` INT NOT NULL,
   PRIMARY KEY (`tweet_id`, `user_id`),
