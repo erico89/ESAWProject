2200a42822eee3dc74f0862bed9f6c512fb4e3f2
CristinaMendoza01 <cristina.mendoza01@estudiant.upf.edu>
2022-06-20 17:16:47 +0200

    EditProfile arreglado

--- Lab4/src/main/java/controllers/EditProfile.java
+++ Lab4/src/main/java/controllers/EditProfile.java
@@ -17,6 +17,10 @@ import org.apache.commons.lang3.tuple.Pair;
 import managers.ManageUsers;
 import models.User;
 
+/**
+ * Servlet implementation class dTcontroller
+ */
+@WebServlet("/EditProfile")
 public class EditProfile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
